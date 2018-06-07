7efaf52e42765f50be72a5b8e1c748ec473a7262
Lejla Kesko <lejla.kesko@hotmail.com>
2018-06-07 16:11:24 +0200

    Add arguments

--- scrape_board_topics.py
+++ scrape_board_topics.py
@@ -3,39 +3,68 @@ import bitcointalk
 import logging
 import memoizer
 import traceback
+import getopt
+import sys
 
-boardId = 14
+def main(argv):
+    if len(argv) == 0:
+        print('You must pass some parameters. Use \"-h\" to help.')
+        return
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s %(levelname)s:%(message)s',
-    datefmt='%m/%d/%Y %I:%M:%S %p')
+    try:
+        opts, args = getopt.getopt(argv, "", (
+            "boardId=",
+            "everyn="
+        ))
 
-logging.info("Beginning scrape of board ID {0}".format(boardId))
-board = memoizer.scrapeBoard(boardId)
-logging.info("Found {0} topic pages in board".format(
-    board['num_pages']))
+        boardId = None
+        everyN = None
 
-for boardPageNum in range(1, board['num_pages'] + 1):
-    topicIds = memoizer.scrapeTopicIds(boardId, boardPageNum)
-    for topicId in topicIds:
-        try:
-            topic = memoizer.scrapeTopic(topicId)
-        except Exception as e:
-            print('-'*60)
-            print("Could not request URL for topic {0}:".format(topicId))
-            print(traceback.format_exc())
-            print('-'*60)
-            logging.info(">>Could not request URL for topic {0}:".format(
-                topicId))
-            continue
-        for topicPageNum in range(1, topic['num_pages'] + 1):
-            messages = memoizer.scrapeMessages(topic['id'], topicPageNum)
-            for message in messages:
-                # if message['member'] > 0:
-                    # memoizer.scrapeMember(message['member'])
-                print(message);
+        for opt, arg in opts:
+            if opt == "--boardId":
+                boardId = int(arg)
+
+            elif opt == '--everyn' and arg != '':
+                everyN = int(arg)
+
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s %(levelname)s:%(message)s',
+            datefmt='%m/%d/%Y %I:%M:%S %p')
+
+        logging.info("Beginning scrape of board ID {0}".format(boardId))
+        board = memoizer.scrapeBoard(boardId)
+        logging.info("Found {0} topic pages in board".format(
+            board['num_pages']))
+
+        for boardPageNum in range(1, board['num_pages'] + 1):
+            topicIds = memoizer.scrapeTopicIds(boardId, boardPageNum)
+            for topicId in topicIds:
+                try:
+                    topic = memoizer.scrapeTopic(topicId)
+                except Exception as e:
+                    print('-'*60)
+                    print("Could not request URL for topic {0}:".format(topicId))
+                    print(traceback.format_exc())
+                    print('-'*60)
+                    logging.info(">>Could not request URL for topic {0}:".format(
+                        topicId))
+                    continue
+                for topicPageNum in range(1, topic['num_pages'] + 1):
+                    messages = memoizer.scrapeMessages(topic['id'], topicPageNum)
+                    for message in messages:
+                        # if message['member'] > 0:
+                            # memoizer.scrapeMember(message['member'])
+                        print(message)
+
+        logging.info("All done.")
+        logging.info("Made {0} requests in total.".format(bitcointalk.countRequested))
+
+    except Exception as argv:
+        print('Arguments parser error' + argv)
+    finally:
+        pass
 
 
-logging.info("All done.")
-logging.info("Made {0} requests in total.".format(bitcointalk.countRequested))
+if __name__ == '__main__':
+    main(sys.argv[1:])
